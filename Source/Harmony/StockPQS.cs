7a0a9d63bf8ac6624784e63ff84f5e7c80c98176
Clayel <videogamer0907@gmail.com>
2025-10-13 21:36:41 -0400

    add loading of custom settings

--- Source/Harmony/StockPQS.cs
+++ Source/Harmony/StockPQS.cs
@@ -1,21 +1,31 @@
 using HarmonyLib;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace AdvancedPQSTools.Harmony
 {
     [HarmonyPatch(typeof(PQS))]
-    internal class Stock_PQS_Patches
+    internal class StockPQSPatches
     {
+        internal static string Body = "";
+        internal static int? minLevel = null;
+        internal static int? maxLevel = null;
+        internal static double? minDetailDistance = null;
+
         [HarmonyPatch("StartSphere")] // cant seem to patch Reset(), it seems to be different from other unity methods like Start()
         [HarmonyPrefix]
         internal static void Prefix_StartSphere(PQS __instance, bool force)
         {
-            if (__instance.name != "Kerbin") // TODO: make this adjustable
+            if (Body == "" || __instance.name != Body)
                 return;
 
-            // TODO: add minLevel
-            __instance.maxLevel = 16; // TODO: make this adjustable
-            __instance.minDetailDistance = 16f; // TODO: make this adjustable
+            if (minLevel.HasValue)
+                __instance.minLevel = minLevel.Value;
+            if (maxLevel.HasValue)
+                __instance.maxLevel = maxLevel.Value;
+            if (minDetailDistance.HasValue)
+                __instance.minDetailDistance = minDetailDistance.Value;
 
             if (PQSCache.PresetList != null)
             {
@@ -23,13 +33,30 @@ namespace AdvancedPQSTools.Harmony
                 if (preset != null)
                 {
                     preset.minDistance = __instance.minDetailDistance;
-                    // TODO: add minSubdivision
+                    preset.minSubdivision = __instance.minLevel;
                     preset.maxSubdivision = __instance.maxLevel;
                     // StartSphere uses the presets if the presets aren't null, so we can preset them here
                 }
             }
 
-            Debug.Log($"[Stock PQS Patches] Prefix_StartSphere applied for {__instance.name}: maxLevel={__instance.maxLevel}, minDetailDistance={__instance.minDetailDistance}");
+            Debug.Log($"[StockPQSPatches] Prefix_StartSphere applied for {__instance.name}: maxLevel={__instance.maxLevel}, minDetailDistance={__instance.minDetailDistance}");
+        }
+
+        internal static void LoadSettings()
+        {
+            ConfigNode settings = GameDatabase.Instance.GetConfigNodes("ADVANCEDPQSTOOLS").FirstOrDefault();
+            if (settings != null)
+            {
+                settings.TryGetValue("Body", ref Body);
+                if (settings.HasValue("minLevel") && int.TryParse(settings.GetValue("minLevel"), out int parsedMin))
+                    minLevel = parsedMin;
+
+                if (settings.HasValue("maxLevel") && int.TryParse(settings.GetValue("maxLevel"), out int parsedMax))
+                    maxLevel = parsedMax;
+
+                if (settings.HasValue("minDetailDistance") && double.TryParse(settings.GetValue("minDetailDistance"), out double parsedDistance))
+                    minDetailDistance = parsedDistance;
+            }
         }
     }
 }
--- Source/HarmonyPatcher.cs
+++ Source/HarmonyPatcher.cs
@@ -1,12 +1,14 @@
 ﻿using UnityEngine;
 
-namespace AdvancedPQSTools
+namespace AdvancedPQSTools.Harmony
 {
     [KSPAddon(KSPAddon.Startup.Instantly, true)]
     public class HarmonyPatcher : MonoBehaviour
     {
         internal void Start()
         {
+            StockPQSPatches.LoadSettings();
+
             var harmony = new HarmonyLib.Harmony("AdvancedPQSTools.HarmonyPatcher");
             harmony.PatchAll();
         }
