9de1bb776016fc56079b3c2e8f02fa49c8074c09
Clayel <videogamer0907@gmail.com>
2025-12-13 19:20:15 -0500

    add settingsPresent bool

--- Source/Harmony/StockPQS.cs
+++ Source/Harmony/StockPQS.cs
@@ -18,12 +18,16 @@ namespace AdvancedPQSTools.Harmony
 
         internal static List<PQSSettings> Settings = new List<PQSSettings>();
         internal static bool settingsLoaded = false;
+        internal static bool settingsPresent = false;
 
         [HarmonyPatch("StartSphere")] // cant seem to patch Reset(), it seems to be different from other unity methods like Start()
         [HarmonyPrefix]
         internal static void Prefix_StartSphere(PQS __instance, bool force)
         {
             LoadSettings(); // we cant do this at KSPAddon.Startup.Instantly bc GameDatabase doesnt seem to be loaded yet
+
+            if (!settingsPresent)
+                return;
 
             PQSSettings match = Settings.FirstOrDefault(m => m.Body == __instance.name);
             if (match == null || match.Body == "")
@@ -65,34 +69,39 @@ namespace AdvancedPQSTools.Harmony
             if (root == null)
             {
                 //Debug.LogWarning("[StockPQSPatches] No ADVANCEDPQSTOOLS config found.");
+                settingsPresent = false; // this should already be set anyway
                 return;
+            }
+            else             
+            {
+                settingsPresent = true;
             }
 
             foreach (ConfigNode node in root.GetNodes("Body"))
-            {
-                PQSSettings setting = new PQSSettings();
-
-                node.TryGetValue("name", ref setting.Body);
-
-                if (node.HasValue("minLevel") && int.TryParse(node.GetValue("minLevel"), out int parsedMin))
-                    setting.minLevel = parsedMin;
-
-                if (node.HasValue("maxLevel") && int.TryParse(node.GetValue("maxLevel"), out int parsedMax))
-                    setting.maxLevel = parsedMax;
-
-                if (node.HasValue("minDetailDistance") && double.TryParse(node.GetValue("minDetailDistance"), out double parsedDistance))
-                    setting.minDetailDistance = parsedDistance;
-
-                if (!string.IsNullOrEmpty(setting.Body))
                 {
-                    //Debug.Log($"[StockPQSPatches] Loaded config for body: {setting.Body}");
-                    Settings.Add(setting);
+                    PQSSettings setting = new PQSSettings();
+
+                    node.TryGetValue("name", ref setting.Body);
+
+                    if (node.HasValue("minLevel") && int.TryParse(node.GetValue("minLevel"), out int parsedMin))
+                        setting.minLevel = parsedMin;
+
+                    if (node.HasValue("maxLevel") && int.TryParse(node.GetValue("maxLevel"), out int parsedMax))
+                        setting.maxLevel = parsedMax;
+
+                    if (node.HasValue("minDetailDistance") && double.TryParse(node.GetValue("minDetailDistance"), out double parsedDistance))
+                        setting.minDetailDistance = parsedDistance;
+
+                    if (!string.IsNullOrEmpty(setting.Body))
+                    {
+                        //Debug.Log($"[StockPQSPatches] Loaded config for body: {setting.Body}");
+                        Settings.Add(setting);
+                    }
+                    else
+                    {
+                        //Debug.LogWarning($"[StockPQSPatches] Config empty");
+                    }
                 }
-                else
-                {
-                    //Debug.LogWarning($"[StockPQSPatches] Config empty");
-                }
-            }
 
             if (Settings.Count == 0)
                 Debug.LogWarning("[StockPQSPatches] Config node found, but no bodies found.");
